2a9a849f2305e5cc50b7540d9c360d316d8396bc
ramdhanyA <ariqathallah38@gmail.com>
2023-01-18 14:24:42 +0800

    Added classify result modal

--- pages/classify/index.tsx
+++ pages/classify/index.tsx
@@ -13,17 +13,20 @@ export default function Classify() {
     const [absence, setAbsence] = useState(0)
     const [g1, setG1] = useState(0)
     const [g2, setG2] = useState(0)
+    const [showModal, setShowModal] = useState(false)
+    const [grade, setGrade] = useState()
 
 
     const weekendAlcoholCategories = ["No alcohol", "1 to 3 shots", "3 to 5 shots", "5 to 8 shots", "More than 8 shots"];
     const feduCategories = ["No education", "Primary school", "Lower secondary (6th to 9th grade)", "Higher secondary (10th to 12th grade)", "Higher education"]
 
 
-    function submitData(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault()
+    function submitData(e: any) {
+        e.preventDefault();
+        setShowModal(true);
         axios.post("https://grade-predictor-api.azurewebsites.net/classify", { g1, g2, absence, walc, fedu })
             .then((res) => {
-                console.log("The response is ", res);
+                setGrade(res.data['grade']);
             })
     }
 
@@ -41,7 +44,7 @@ export default function Classify() {
             <div className="m-auto mt-[2vh] w-full max-w-md rounded-lg border border-gray-200 bg-white bg-gradient-to-r from-pink-500 to-yellow-500 p-4 shadow-md sm:p-6 md:p-8 mb-36 ">
                 <form
                     className="space-y-6"
-                    onSubmit={(e) => submitData(e)}
+                // onSubmit={(e) => submitData(e)}
                 >
                     <GradeInput
                         title="g1"
@@ -80,15 +83,43 @@ export default function Classify() {
                     />
 
                     <button
-                        type="submit"
+                        onClick={(e) => submitData(e)}
+                        data-modal-target="small-modal" data-modal-toggle="small-modal"
                         className="w-full from-cyan-500 to-blue-500 hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 rounded-lg bg-gradient-to-r  px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
                     >
                         Submit
                     </button>
                 </form>
+                {showModal ? <Modal grade={grade} setShowModal={setShowModal} /> : null}
+
             </div >
         </>
     );
+}
+
+function Modal(props: { setShowModal: Function, grade: String }) {
+    const { setShowModal, grade } = props;
+    return (
+        <div id="small-modal" className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+            <div className="relative w-full h-full max-w-md md:h-auto mx-auto">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                            Small modal
+                        </h3>
+                        <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="small-modal">
+                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <h1>{grade}</h1>
+                    <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                        <button onClick={(e) => setShowModal(false)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">OK</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 function GradeInput(props: { title: string, desc: string, max: string, state: any, setStateFunc: Function }) {
