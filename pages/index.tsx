2dcf89e07a3b66c7b673be2dc3272e74e4d5f9b2
ramdhanyA <ariqathallah38@gmail.com>
2023-01-24 19:01:36 +0700

    Added backend integration for the regression

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ export default function Home() {
   const [travelTime, setTravelTime] = useState("")
   const [health, setHealth] = useState("")
   const [walc, setWalc] = useState("")
+  const [showModal, setShowModal] = useState(false)
+  const [grade, setGrade] = useState("")
 
 
   const studyTimeCategories = ["Less than 2 hours", "2 to 5 hours", "5 to 10 hours", "More than 10 hours"];
@@ -24,7 +26,7 @@ export default function Home() {
     e.preventDefault()
     axios.post("https://grade-predictor-api.azurewebsites.net/regress", { studyTime, freeTime, travelTime, health, walc })
       .then((res) => {
-        console.log("The response is ", res);
+        setGrade(res.data['grade']);
       })
   }
 
@@ -92,11 +94,36 @@ export default function Home() {
             Submit
           </button>
         </form>
+        {showModal ? <Modal grade={grade} setShowModal={setShowModal} /> : null}
       </div >
     </>
   );
 }
 
+function Modal(props: { setShowModal: Function, grade: String }) {
+  const { setShowModal, grade } = props;
+  return (
+    <div id="small-modal" className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+      <div className="relative w-full h-full max-w-md md:h-auto mx-auto">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+              Small modal
+            </h3>
+            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="small-modal">
+              <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+          </div>
+          <h1>{grade}</h1>
+          <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+            <button onClick={(e) => setShowModal(false)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 
 function RadioList(props: { categories: Array<string>, title: string, state: string, setStateFunc: Function, desc: string }): JSX.Element {
