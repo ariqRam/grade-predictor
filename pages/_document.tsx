9bf69c7a58730bccf827c80e870dcd00ba143495
ramdhanyA <ariqathallah38@gmail.com>
2023-01-24 20:05:33 +0700

    Added data set source

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,9 @@ export default function Document() {
       <Head />
       <body className=" bg-gradient-to-r from-[#12c2e9] via-[#c471ed] to-[#f64f59] background-animate">
         <Main />
+        <div className="text-center pb-20">
+          <p>Trained on the <a className="underline" href="https://archive.ics.uci.edu/ml/datasets/student+performance">UCI Machine Learning Repository</a></p>
+        </div>
         <NextScript />
       </body>
     </Html>
